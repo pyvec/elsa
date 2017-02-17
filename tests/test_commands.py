3400adb0d984ff87082e758a26e1a6bf6385581e
Miro Hrončok <miro@hroncok.cz>
2017-02-17 14:56:14 +0100

    Tests: Use a context manager for background runs

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -1,3 +1,4 @@
+from contextlib import contextmanager
 import os
 import signal
 import shutil
@@ -42,31 +43,26 @@ protocol = parametrized_fixture('http', 'https')
 
 class ElsaRunner:
     '''
-    Class for elsa fixture enabling multiple runs and exit status check
+    Class for elsa fixture enabling blocking or background runs
     '''
-    def __init__(self):
-        self.runs = []
-
     def run(self, command):
         print('COMMAND: python website.py', command)
+        subprocess.run([sys.executable, WEBSITE] + command.split())
+
+    @contextmanager
+    def run_bg(self, command):
+        print('COMMAND IN BACKGROUND: python website.py', command)
         proc = subprocess.Popen([sys.executable, WEBSITE] + command.split())
-        if 'serve' in command:
-            time.sleep(1)
-        else:
-            proc.communicate()
-        self.runs.append((proc, command))
+        time.sleep(1)  # TODO actually check if ready instead
+        yield proc
+        proc.terminate()
+        proc.communicate()
+        assert (proc.returncode == -signal.SIGTERM or
+                proc.returncode == 0)
 
     def finalize(self):
-        try:
-            for proc, command in self.runs:
-                proc.terminate()
-                if 'serve' in command:
-                    proc.communicate()
-                assert (proc.returncode == 0 or
-                        proc.returncode == -signal.SIGTERM)
-        finally:
-            self.lax_rmtree(BUILDDIR)
-            self.lax_rmbranch('gh-pages')
+        self.lax_rmtree(BUILDDIR)
+        self.lax_rmbranch('gh-pages')
 
     @classmethod
     def lax_rmtree(cls, path):
@@ -87,7 +83,7 @@ class ElsaRunner:
 def elsa():
     er = ElsaRunner()
     try:
-        yield er.run
+        yield er
     finally:
         er.finalize()
 
@@ -99,55 +95,55 @@ def commit_info():
 
 
 def test_serve(elsa):
-    elsa('serve')
-    assert 'SUCCESS' in requests.get('http://localhost:8003/').text
+    with elsa.run_bg('serve'):
+        assert 'SUCCESS' in requests.get('http://localhost:8003/').text
 
 
 def test_port(elsa, port, serve_command):
-    elsa(serve_command + ' --port {}'.format(port))
-    assert 'SUCCESS' in requests.get('http://localhost:{}/'.format(port)).text
+    with elsa.run_bg(serve_command + ' --port {}'.format(port)):
+        url = 'http://localhost:{}/'.format(port)
+        assert 'SUCCESS' in requests.get(url).text
 
 
 def test_cname(elsa, cname, serve_command):
     code = 200 if is_cname(cname) else 404
 
-    elsa(serve_command + cname)
-    assert requests.get('http://localhost:8003/CNAME').status_code == code
+    with elsa.run_bg(serve_command + cname):
+        assert requests.get('http://localhost:8003/CNAME').status_code == code
 
 
 def test_freeze(elsa):
-    elsa('freeze')
+    elsa.run('freeze')
     with open(INDEX) as f:
         assert 'SUCCESS' in f.read()
 
 
 def test_freeze_cname(elsa):
-    elsa('freeze')
+    elsa.run('freeze')
     with open(CNAME) as f:
         assert f.read().strip() == 'example.org'
 
 
 def test_freeze_no_cname(elsa):
-    elsa('freeze --no-cname')
+    elsa.run('freeze --no-cname')
     assert not os.path.exists(CNAME)
 
 
 def test_freeze_base_url(elsa, protocol, domain):
-    elsa('freeze --base-url {}://{}'.format(protocol, domain))
+    elsa.run('freeze --base-url {}://{}'.format(protocol, domain))
     with open(CNAME) as f:
         assert f.read().strip() == domain
 
 
 def test_freeze_serve(elsa):
-    elsa('freeze --serve')
-    with open(INDEX) as f:
+    with elsa.run_bg('freeze --serve'), open(INDEX) as f:
         assert 'SUCCESS' in f.read()
         assert 'SUCCESS' in requests.get('http://localhost:8003/').text
 
 
 def test_freeze_path(elsa, tmpdir, cname):
     path = tmpdir.join('foo')
-    elsa('freeze --path {}{}'.format(path, cname))
+    elsa.run('freeze --path {}{}'.format(path, cname))
 
     assert path.check(dir=True)
     assert path.join('index.html').check(file=True)
@@ -155,14 +151,14 @@ def test_freeze_path(elsa, tmpdir, cname):
 
 
 def test_deploy_no_push_files(elsa, cname):
-    elsa('deploy --no-push' + cname)
+    elsa.run('deploy --no-push' + cname)
     with open(INDEX) as f:
         assert 'SUCCESS' in f.read()
         assert is_cname(cname) == os.path.exists(CNAME)
 
 
 def test_deploy_no_push_git(elsa, cname):
-    elsa('deploy --no-push' + cname)
+    elsa.run('deploy --no-push' + cname)
     commit = commit_info()
 
     assert '.nojekyll' in commit
@@ -179,8 +175,8 @@ def test_freeze_and_deploy(elsa, tmpdir, path):
     freeze_command = 'freeze' + args
     deploy_command = 'deploy --no-push' + args
 
-    elsa(freeze_command)
-    elsa(deploy_command)
+    elsa.run(freeze_command)
+    elsa.run(deploy_command)
 
     commit = commit_info()
     assert 'index.html' in commit
