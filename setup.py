dfa23918e1ae13af79e1555bdd9a4dd939c67e5d
Miro Hrončok <miro@hroncok.cz>
2017-02-22 13:47:42 +0100

    Remove the Python 3.7 classifier, as it is not valid

--- setup.py
+++ setup.py
@@ -50,7 +50,6 @@ setup(
         'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
         'Topic :: Internet :: WWW/HTTP',
     ]
 )
